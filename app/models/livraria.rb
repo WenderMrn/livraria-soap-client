455e39679b19afbf2000e9d00a56edc6f2a6f812
Wender Mrn <wender.mrn@gmail.com>
2017-08-19 12:32:30 -0300

    [UPDATE] ajustando controller e view e criando demais telas

--- app/models/livraria.rb
+++ app/models/livraria.rb
@@ -5,18 +5,67 @@ class Livraria
 	
 	global :open_timeout, 30
 
-	operations :listar_livros, :pesquisar_livro_por_codigo, :cadastrar_livro
+	operations :listar_livros, :procurar_por_isbn, :procurar_por_id,:pesquisar_por_isbn,
+             :cadastrar_livro, :excluir_livro, :atualizar_livro           
 
-	def cadastrar_livros(livro)
-    super(message: livro).body[:cadastrar_livros_response][:return]
+	def cadastrar_livro(livro)
+    puts "livro aqui#{livro.to_json}"
+    super(message:livro_to_hash(livro)).body[:cadastrar_livro_response][:return]
   end
 
   def listar_livros
-    super().body[:listar_livros_response][:return]
+    @livros = super().body[:listar_livros_response][:return]
+    puts "AQUI: #{@livros.class}"
+    if @livros.is_a?(Array)
+      @livros
+    elsif not @livros.nil?
+      @livros = Array.new <<  @livros
+    else
+      @livros = Array.new
+    end
   end
 
-  def pesquisar_livro_por_codigo(codigo)
-    super(message:{codigo: codigo}).body[:pesquisar_livro_por_codigo_response][:return]
+  def procurar_por_isbn(isbn)
+    Livro.new(super(message:{isbn: isbn}).body[:procurar_por_isbn_response][:return])
   end
+
+  def pesquisar_por_isbn(isbn)
+    @livros = super(message:{isbn: isbn}).body[:pesquisar_por_isbn_response][:return]
+    if @livros.is_a?(Array)
+      @livros.to_json
+    elsif not @livros.nil?
+      @livros = Array.new << @livros
+      @livros.to_json  
+    else
+      Array.new.to_json
+    end
+  end
+
+  def procurar_por_id(id)
+    Livro.new(super(message:{id: id}).body[:procurar_por_id_response][:return])
+  end
+
+  def excluir_livro(id)
+    Livro.new(super(message:{id: id}).body[:excluir_livro_response][:return])
+  end
+
+  def atualizar_livro(livro)
+    super(message:livro_to_hash(livro)).body[:atualizar_livro_response][:return]
+  end
+
+  private
+    def livro_to_hash(livro)    
+      {
+        livro: {
+          id:      (not livro.id.nil? ) ? livro.id.to_i : 0, 
+          codigo:  livro.codigo,
+          titulo:  livro.titulo.upcase,
+          editora: livro.editora,
+          isbn:    livro.isbn,  
+          edicao:  livro.edicao, 
+          autor:   livro.autor
+        }
+      }
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'livraria/livro/:codigo', to: 'livrarias#show'
-  root "livrarias#index"
+  resources :livros
+  post 'livros/search', to: 'livros#search'
+  root "livros#index"
 end
